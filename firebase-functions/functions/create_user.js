2290629e67e6721b813b858e4a2f18496b6a78cc
Brian Fajardo <bmf5@hotmail.com>
2017-07-09 16:38:23 -0400

    Create cloud function createUser

---
+++ firebase-functions/functions/create_user.js
@@ -0,0 +1,18 @@
+const admin = require('firebase-admin')
+
+const createUser = (req, res) => {
+
+  if (!req.body.phoneNumber) {
+    return res.send({ error: 'Phone number is required.' }).status(422)
+  }
+
+  // Format phone number to remove any non digit character.
+  const phoneNumber = String(req.body.phoneNumber).replace(/[^\d]/g, "")
+
+  // Using user's phone number as their uid
+  admin.auth().createUser({ uid: phoneNumber })
+    .then(user => res.send(user).status(200))
+    .catch(err => res.send({ error: err }).status(422))
+}
+
+module.exports = createUser
--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,8 +1,12 @@
-const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+const serviceAccount = require('./service_account.json')
+const createUser = require('./create_user')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://rn-otp-auth.firebaseio.com"
+})
+
+exports.createUser = functions.https.onRequest(createUser)
