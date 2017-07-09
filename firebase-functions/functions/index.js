cd2b2dfc87df414465d45e0da2c12974d6b90ee8
Brian Fajardo <bmf5@hotmail.com>
2017-07-09 18:04:45 -0400

    Verify user provided code from Twilio text. On successful authentication, provide user with JWT

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -4,11 +4,14 @@ const functions = require('firebase-functions')
 const serviceAccount = require('./service_account.json')
 const createUser = require('./create_user')
 const requestOneTimePassword = require('./request_otp')
+const verifyOneTimePassword = require('./verify_otp')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://rn-otp-auth.firebaseio.com"
 })
 
+// Wrapping controllers with Firebase's functions' methods.
 exports.createUser = functions.https.onRequest(createUser)
 exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword)
+exports.verifyOneTimePassword = functions.https.onRequest(verifyOneTimePassword)
---
+++ firebase-functions/functions/verify_otp.js
@@ -0,0 +1,32 @@
+const admin = require('firebase-admin')
+
+const verifyOneTimePassword = (req, res) => {
+  const { phoneNumber, code } = req.body
+  if (!phoneNumber || !code) {
+    return res.send({ error: 'Phone number and code is required.' }).status(422)
+  }
+
+  const userProvidedNumber = String(phoneNumber).replace(/[^\d]/g, '')
+  const userProvidedCode = parseInt(code)
+
+  admin.auth().getUser(userProvidedNumber)
+    .then(() => {
+      const ref = admin.database.ref(`users/${userProvidedNumber}`)
+      // Look at users collection and phone number as the key.
+      // When a reference is found, call callback with a snapshot of the node.
+      ref.on('value', (snapshot) => {
+        // database.ref() is actively still searching, once a ref is found,
+        // do not attempt to listen to anymore value changes.
+        ref.off()
+        const user = snapshot.val()
+        if (userProvidedCode !== user.code || !user.isValid) {
+          return res.send({ error: 'Invalid code' }).status(422)
+        }
+        // Flip valid code flag and then send a JSON web token.
+        ref.update({ isValid: false })
+        admin.auth().createCustomToken(userProvidedNumber)
+          .then(token => res.send(token).status(200))
+      })
+    })
+    .catch(err => res.send({ error: err }).status(422))
+}
