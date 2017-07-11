5d597a960dfda0989edf2359f0ac7869a6ab7cef
Brian Fajardo <bmf5@hotmail.com>
2017-07-11 17:17:54 -0400

    Implement Firebase authentication on successful login

--- mobile/src/Main.js
+++ mobile/src/Main.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View } from 'react-native'
 import { Divider } from 'react-native-elements'
+import firebase from 'firebase'
 
 import SignUpFormContainer from './containers/SignUpFormContainer'
 import SignInFormContainer from './containers/SignInFormContainer'
 
-const Main = () => (
-  <View style={{ flex: 1, backgroundColor: 'white' }}>
-    <SignUpFormContainer />
-    <Divider style={{ backgroundColor: 'red' }} />
-    <SignInFormContainer />
-  </View>
-)
+class Main extends Component {
+
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyA5z6sND7dAH92W2A1VHXWxMkvBvQSSFN0',
+      authDomain: 'rn-otp-auth.firebaseapp.com',
+      databaseURL: 'https://rn-otp-auth.firebaseio.com',
+      projectId: 'rn-otp-auth',
+      storageBucket: 'rn-otp-auth.appspot.com',
+      messagingSenderId: '56819372949'
+    }
+    firebase.initializeApp(config)
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: 'white' }}>
+        <SignUpFormContainer />
+        <Divider style={{ backgroundColor: 'red' }} />
+        <SignInFormContainer />
+      </View>
+    )
+  }
+}
 
 export default Main
--- mobile/src/actions/index.js
+++ mobile/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import firebase from 'firebase'
 
 const ROOT_URL = 'https://us-central1-rn-otp-auth.cloudfunctions.net'
 
@@ -12,5 +13,14 @@ export const signUp = phoneNumber => async (dispatch) => {
 }
 
 export const signIn = (phoneNumber, code) => async (dispatch) => {
-  console.log('sign in form action creator!')
+  try {
+    // Using let instead of const because of the async nature.
+    // For example, using const, initially const data = null,
+    // when Promise is resolved, data = response.data.token and using const
+    // means immutability. Therefore, we use let for the flexibility it offers.
+    let { data: token } = await axios.post(`${ROOT_URL}/verifyOneTimePassword`, { phoneNumber, code })
+    firebase.auth().signInWithCustomToken(token)
+  } catch (err) {
+    dispatch({ type: 'GENERATED_ERROR', payload: err })
+  }
 }
